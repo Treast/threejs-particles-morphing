2e8884eaf91524acb034c12bae9faf1802ce5106
Treast <vincent.riva@outlook.fr>
2019-12-17 07:46:00 +0100

    Fix loading object and add interface

--- src/app/js/Scene.ts
+++ src/app/js/Scene.ts
@@ -1,8 +1,23 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { OBJLoader2 } from 'three/examples/jsm/loaders/OBJLoader2';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 import { GeometryUtils } from 'three/examples/jsm/utils/GeometryUtils';
 import * as dat from 'dat.gui';
+
+interface Model {
+  name: string;
+  vertices: THREE.Vector3[];
+  rotation: THREE.Vector3;
+}
+
+interface ParticleSystem {
+  geometry: THREE.BufferGeometry;
+  mesh: THREE.Points;
+  material: THREE.Material;
+  count: number;
+  model: Model;
+  modelIndex: number;
+}
 
 class Scene {
   private scene: THREE.Scene;
@@ -11,11 +26,11 @@ class Scene {
 
   private controls: OrbitControls;
 
-  private orientation: THREE.Vector3 = new THREE.Vector3(0, 0, 0);
-  private gui: dat.GUI;
-
   private pSystem: THREE.Points;
   private pointsGeometry: THREE.BufferGeometry;
+
+  private models: Model[];
+  private particleSystem: ParticleSystem;
 
   constructor() {
     this.scene = new THREE.Scene();
@@ -25,12 +40,19 @@ class Scene {
     document.body.appendChild(this.renderer.domElement);
 
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-    // this.gui = new dat.GUI();
+
     this.bind();
   }
 
   bind() {
-    window.addEventListener('resize', () => this.onResize);
+    window.addEventListener('resize', () => this.onResize());
+    window.addEventListener('click', () => {
+      console.log(this.particleSystem);
+      // const index = (this.particleSystem.modelIndex + 1) % this.models.length;
+      // console.log(index);
+      // this.particleSystem.modelIndex = index;
+      // this.particleSystem.model = this.models[index];
+    });
   }
 
   onResize() {
@@ -42,91 +64,129 @@ class Scene {
   init() {
     this.camera.position.set(0.5, 0.5, 0.5);
     this.camera.lookAt(0, 0, 0);
+    this.models = [
+      {
+        name: 'assets/Bunny.obj',
+        vertices: [],
+        rotation: new THREE.Vector3(4.6, 6.2, 0),
+      },
+      {
+        name: 'assets/Bison.obj',
+        vertices: [],
+        rotation: new THREE.Vector3(0, 3.14, 0),
+      },
+    ];
 
-    // this.gui.add(this.orientation, 'x', 0, Math.PI * 2).step(0.05);
-    // this.gui.add(this.orientation, 'y', 0, Math.PI * 2).step(0.05);
-    // this.gui.add(this.orientation, 'z', 0, Math.PI * 2).step(0.05);
+    this.particleSystem = {
+      geometry: null,
+      material: null,
+      mesh: null,
+      count: 2000,
+      model: null,
+      modelIndex: 0,
+    };
 
     const ambientLight = new THREE.AmbientLight(0xcccccc, 1.4);
     this.scene.add(ambientLight);
 
-    const mat = new THREE.MeshNormalMaterial();
-    const vertices: THREE.Vector3[] = [];
-    const objLoader = new OBJLoader2();
-    objLoader.load('assets/Bunny.obj', root => {
-      root.traverse(item => {
-        if (item instanceof THREE.Mesh) {
-          item.material = mat;
+    const objLoader = new OBJLoader();
+    this.models.forEach((model, modelIndex) => {
+      objLoader.load(model.name, root => {
+        console.log('Load: ', model.name);
+        const vertices: THREE.Vector3[] = [];
+        root.traverse(item => {
+          if (item instanceof THREE.Mesh) {
+            item.geometry.rotateX(model.rotation.x);
+            item.geometry.rotateY(model.rotation.y);
+            item.geometry.rotateZ(model.rotation.z);
 
-          item.geometry.rotateX(4.6);
-          item.geometry.rotateY(6.2);
+            const box = new THREE.Box3().setFromObject(item);
 
-          if (item.geometry instanceof THREE.Geometry) {
-            const v = GeometryUtils.randomPointsInGeometry(item.geometry, 200);
-            vertices.push(...v);
+            const translateX = -(box.max.x - box.min.x) / 2;
+            const translateZ = -(box.max.z - box.min.z) / 2;
+            item.geometry.translate(translateX, 0, translateZ);
+
+            const scaleX = box.max.x - box.min.x;
+            const scaleY = box.max.y - box.min.y;
+            const scaleZ = box.max.z - box.min.z;
+
+            const scale = Math.max(scaleX, scaleY, scaleZ);
+            item.geometry.scale(1 / scale, 1 / scale, 1 / scale);
+
+            if (item.geometry instanceof THREE.Geometry) {
+              vertices.push(...item.geometry.vertices);
+            }
+
+            if (item.geometry instanceof THREE.BufferGeometry) {
+              const geo = new THREE.Geometry().fromBufferGeometry(item.geometry);
+              vertices.push(...geo.vertices);
+            }
+
+            // const mat = new THREE.MeshNormalMaterial();
+            // const m = new THREE.Mesh(item.geometry, mat);
+            // this.scene.add(m);
           }
+        });
 
-          if (item.geometry instanceof THREE.BufferGeometry) {
-            const geo = new THREE.Geometry().fromBufferGeometry(item.geometry);
-            const v = GeometryUtils.randomPointsInGeometry(geo, 200);
-            vertices.push(...v);
-          }
+        const rootGeometry = new THREE.BufferGeometry().setFromPoints(vertices);
+        model.vertices = GeometryUtils.randomPointsInBufferGeometry(rootGeometry, this.particleSystem.count);
+        model.vertices = vertices;
 
-          const box = new THREE.Box3().setFromObject(item);
-
-          this.createParticles(vertices, box);
+        if (!this.particleSystem.model) {
+          this.particleSystem.model = model;
+          this.particleSystem.modelIndex = modelIndex;
+          this.createParticles();
         }
       });
     });
   }
 
-  createParticles(vertices: THREE.Vector3[], box: THREE.Box3) {
-    this.pointsGeometry = new THREE.BufferGeometry();
+  createParticles() {
+    this.particleSystem.geometry = new THREE.BufferGeometry();
     const positions: number[] = [];
-    const colors: number[] = [];
-    const sizes: number[] = [];
 
-    const c = new THREE.Color(0x2fbdf5);
-
-    const scaleX = box.max.x - box.min.x;
-    const scaleY = box.max.y - box.min.y;
-    const scaleZ = box.max.z - box.min.z;
-
-    const scale = Math.max(scaleX, scaleY, scaleZ) * 1.75;
-
-    vertices.forEach(vertex => {
-      positions.push(vertex.x / scale, vertex.y / scale, vertex.z / scale);
-      colors.push(c.r, c.g, c.b);
-      sizes.push(0.001);
+    this.particleSystem.model.vertices.forEach(vertex => {
+      positions.push(vertex.x, vertex.y, vertex.z);
     });
 
-    this.pointsGeometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
+    this.particleSystem.geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
 
-    const pointsMaterial = new THREE.PointsMaterial({
+    this.particleSystem.material = new THREE.PointsMaterial({
       color: 0x2fbdf5,
       size: 0.001,
     });
 
-    this.pSystem = new THREE.Points(this.pointsGeometry, pointsMaterial);
-    const pBox = new THREE.Box3().setFromObject(this.pSystem);
-    this.pSystem.translateX(-(pBox.max.x - pBox.min.x) / 2);
-    this.pSystem.translateZ(-(pBox.max.z - pBox.min.z) / 2);
-    this.scene.add(this.pSystem);
+    this.particleSystem.mesh = new THREE.Points(this.particleSystem.geometry, this.particleSystem.material);
+    this.scene.add(this.particleSystem.mesh);
+  }
+
+  lerp(a: number, b: number, n: number) {
+    return (1 - n) * a + n * b;
   }
 
   render() {
     requestAnimationFrame(() => this.render());
     this.renderer.render(this.scene, this.camera);
 
-    // @ts-ignore
-    const points = this.pSystem.geometry.attributes.position.array;
-    for (let i = 0; i < points.length; i += 1) {
+    if (false && this.particleSystem.mesh) {
       // @ts-ignore
-      points[i] += Math.random() * 0.001 - 0.0005;
-    }
+      const points = this.particleSystem.mesh.geometry.attributes.position.array;
+      for (let i = 0; i < points.length; i += 3) {
+        const index = i % 3;
+        const modelVertex = this.particleSystem.model.vertices[index];
 
-    // @ts-ignore
-    this.pSystem.geometry.attributes.position.needsUpdate = true;
+        if (!modelVertex) {
+          console.log(this.particleSystem.model);
+        }
+
+        points[i] = this.lerp(points[i], modelVertex.x, 0.015);
+        points[i + 1] = this.lerp(points[i + 1], modelVertex.y, 0.015);
+        points[i + 2] = this.lerp(points[i + 2], modelVertex.z, 0.015);
+      }
+
+      // @ts-ignore
+      this.particleSystem.mesh.geometry.attributes.position.needsUpdate = true;
+    }
 
     //this.pSystem.rotation.set(this.orientation.x, this.orientation.y, this.orientation.z, 'XYZ');
 
